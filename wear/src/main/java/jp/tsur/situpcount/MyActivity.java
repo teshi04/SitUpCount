2eaba0cdd69ba68aaac74836682b07bc1c2f2afe
teshi04 <teshi04@gmail.com>
2014-09-05 23:09:57 +0900

    Get acceleration

--- wear/src/main/java/jp/tsur/situpcount/MyActivity.java
+++ wear/src/main/java/jp/tsur/situpcount/MyActivity.java
@@ -1,24 +1,57 @@
 package jp.tsur.situpcount;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 
-public class MyActivity extends Activity {
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
-    private TextView mTextView;
+public class MyActivity extends Activity implements SensorEventListener {
+
+    @InjectView(R.id.text_view)
+    TextView mTextView;
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-            }
-        });
+        ButterKnife.inject(this);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    // Sensor
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // Z 軸の加速度
+        float acceleration = event.values[2];
+        mTextView.setText(String.valueOf((int) acceleration));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
