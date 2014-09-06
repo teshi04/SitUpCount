8c7440b6eebfb8a1a53edd508df7e5832170de0f
teshi04 <teshi04@gmail.com>
2014-09-06 17:08:50 +0900

    Add share menu

--- mobile/src/main/java/jp/tsur/situpcount/ResultActivity.java
+++ mobile/src/main/java/jp/tsur/situpcount/ResultActivity.java
@@ -1,12 +1,14 @@
 package jp.tsur.situpcount;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 
 
 public class ResultActivity extends Activity {
@@ -16,14 +18,24 @@ public class ResultActivity extends Activity {
     @InjectView(R.id.count_label)
     TextView mCountLabel;
 
+    private String mSitupCount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         ButterKnife.inject(this);
 
-        String situpCount = getIntent().getStringExtra(EXTRA_SITUP_COUNT);
-        mCountLabel.setText(situpCount);
+        mSitupCount = getIntent().getStringExtra(EXTRA_SITUP_COUNT);
+        mCountLabel.setText(mSitupCount);
     }
 
+    @OnClick(R.id.share_button)
+    void share() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, "腹筋カウントくん で " + mSitupCount + " 回腹筋をしました。");
+        startActivity(intent);
+    }
 }
