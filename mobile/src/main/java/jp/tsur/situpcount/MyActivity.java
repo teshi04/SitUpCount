233b3dcce4e6dfaabc61912808cea573a32cb49e
teshi04 <teshi04@gmail.com>
2014-09-07 19:44:43 +0900

    ￼:put_litter_in_its_place:

--- mobile/src/main/java/jp/tsur/situpcount/MyActivity.java
+++ mobile/src/main/java/jp/tsur/situpcount/MyActivity.java
@@ -2,9 +2,6 @@ package jp.tsur.situpcount;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
 
 public class MyActivity extends Activity {
 
